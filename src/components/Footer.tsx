a947eed61f14b9986a02d919c0078f40e7685fbd
albarqi19 <89564646+albarqi19@users.noreply.github.com>
2024-12-11 16:09:41 +0300

    تحديث حجم شعار المسجد في تذييل الصفحة

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ export function Footer() {
   return (
     <div className="text-center mt-8">
       <img 
-        src="https://e.top4top.io/p_3267f99ei1.png" 
+        src="https://h.top4top.io/p_3267o2pkr1.png" 
         alt="شعار جامع الشيخ سعيد رداد"
-        className="mx-auto h-16"
+        className="mx-auto h-10"
       />
     </div>
   );
